409b93a1c1f12b9216354c4fcdf88e46fcec6e65
dell <dell@DESKTOP-95P10MB>
2019-06-23 17:47:12 +0530

    Display Title

--- src/main/java/com/edusol/mavenproject/App.java
+++ src/main/java/com/edusol/mavenproject/App.java
@@ -9,11 +9,18 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  */
 public class App 
 {
+	public static WebDriver driver;
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         System.setProperty("webdriver.gecko.driver","E:\\jars\\SeleniumJAR\\geckodriver.exe" );
-        WebDriver driver = new FirefoxDriver();
+         driver = new FirefoxDriver();
         driver.get("https://www.google.com/");
+        displayTitle();
+    }
+    
+    public static void displayTitle()
+    {
+    	System.out.println(driver.getTitle());
     }
 }
